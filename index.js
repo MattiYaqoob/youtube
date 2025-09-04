81fea9f3393649b40ed907122e9b1f695371b897
MattiYaqoob <matishahad2009@yahoo.com>
2025-09-04 21:22:01 +0200

    Add render-build.sh and render.yaml

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { exec } = require('child_process');
+const ffmpegPath = require('ffmpeg-static'); // هذا يحدد مسار ffmpeg تلقائيًا على أي نظام
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,9 +8,7 @@ const port = process.env.PORT || 3000;
 const streamKey = "utuu-gbub-vt43-kgcj-d2f4";
 const streamURL = `rtmps://a.rtmp.youtube.com/live2/${streamKey}`;
 
-
 const ffmpegCommand = `${ffmpegPath} -f lavfi -i color=c=black:s=1280x720:r=30 -c:v libx264 -preset veryfast -b:v 2500k -an -f flv "${streamURL}"`;
-
 
 exec(ffmpegCommand, (err, stdout, stderr) => {
   if (err) {
